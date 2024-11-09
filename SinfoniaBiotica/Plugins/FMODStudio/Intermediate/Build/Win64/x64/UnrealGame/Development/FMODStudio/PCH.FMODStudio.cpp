6bbfba552ec0cbb9bac80ed641b68df72d38f190
b1tdreamer <fer.kzyon@gmail.com>
2024-11-09 21:54:10 +0100

    Recovering the folder

---
+++ SinfoniaBiotica/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealGame/Development/FMODStudio/PCH.FMODStudio.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.41.34123
+#include "C:/Users/b1tdreamer/Documents/UnrealProjects/SinfoniaBiotica/SinfoniaBiotica/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealGame/Development/FMODStudio/PCH.FMODStudio.h"

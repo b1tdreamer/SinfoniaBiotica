3003d0c8fef037911c5017c79ac7c5dff7844d77
PedroRibot <pedro.ribot09@gmail.com>
2024-11-16 01:25:15 +0100

    Cleaning Plugin

--- SinfoniaBiotica/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudioEditor/PCH.FMODStudioEditor.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.36.32545
-#include "C:/AAA_PEDRO/002_FreelanceJobs/20240709_Fer_SinfoniaBiotica/Project/AA_SinfoniaBiotica/SinfoniaBiotica/SinfoniaBiotica/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudioEditor/PCH.FMODStudioEditor.h"
--- SinfoniaBiotica/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealGame/Development/FMODStudio/PCH.FMODStudio.h
+++
@@ -1,3 +0,0 @@
-// PCH for C:/Users/b1tdreamer/Documents/UnrealProjects/SinfoniaBiotica/SinfoniaBiotica/Plugins/FMODStudio/Source/FMODStudio/Private/FMODStudioPrivatePCH.h
-#include "C:/Users/b1tdreamer/Documents/UnrealProjects/SinfoniaBiotica/SinfoniaBiotica/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealGame/Development/FMODStudio/Definitions.h"
-#include "C:/Users/b1tdreamer/Documents/UnrealProjects/SinfoniaBiotica/SinfoniaBiotica/Plugins/FMODStudio/Source/FMODStudio/Private/FMODStudioPrivatePCH.h"
